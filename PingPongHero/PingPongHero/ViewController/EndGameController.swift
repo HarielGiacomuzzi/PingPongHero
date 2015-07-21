1309963e5ff1b40f63ddd59efa2a371c0d4a20b9
brunork26 <bruno.r.kieling@hotmail.com>
2015-07-21 12:42:20 -0300

    end game controller and initial controller added

--- PingPongHero/PingPongHero/ViewController/EndGameController.swift
+++ PingPongHero/PingPongHero/ViewController/EndGameController.swift
@@ -1,25 +1,24 @@
 //
-//  ViewController.swift
+//  EndGameController.swift
 //  PingPongHero
 //
-//  Created by Hariel Giacomuzzi on 7/20/15.
+//  Created by bruno raupp kieling on 7/20/15.
 //  Copyright (c) 2015 Hariel Giacomuzzi. All rights reserved.
 //
 
 import UIKit
 
-class InitialController: UIViewController {
+class EndGameController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
